4c074475b8782b1e737982780360014b0fa1a571
Ruchin Munjal <emailruchin@gmail.com>
2022-07-01 21:00:28 +0100

    new dtos and a new gitignore

--- Play.Catalog/src/Play.Catalog.Service/ModelDtos.cs
+++ Play.Catalog/src/Play.Catalog.Service/ModelDtos.cs
@@ -4,3 +4,6 @@ namespace Play.Catalog.Service.Dtos;
 public record ItemDto(Guid Id,string Name, string Description, decimal Price, DateTimeOffset DateCreated);
 public record CreateItemDto(string Name, string Description, decimal Price);
 
+public record UpdateItemDto(string Name, string Description, decimal Price);
+
+
